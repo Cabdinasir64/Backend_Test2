3fe1eaa0232e547e2c918713e784da7bfc179426
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-27 16:55:51 +0300

    feat: update connect loger file

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import { connectDB } from "./db";
 import dotenv from "dotenv";
+import { logger } from "./Middleware/logger";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+app.use(logger);
+
+const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
