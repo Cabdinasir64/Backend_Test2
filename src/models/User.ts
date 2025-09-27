2e53d73fef7cf726c8345e43bb73e516ca761767
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-27 17:00:16 +0300

    feat: add User model with name, email, password

---
+++ src/models/User.ts
@@ -0,0 +1,21 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+}
+
+const UserSchema = new Schema<IUser>(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+    },
+    {
+        strict: true, 
+    }
+);
+
+const User = model<IUser>("Users", UserSchema);
+export default User;
