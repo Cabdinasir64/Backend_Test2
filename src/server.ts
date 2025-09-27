b020b47536069561cfaf1d6dfddd213548983469
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-27 17:03:38 +0300

    feat: integrate userRoutes into server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { connectDB } from "./db";
 import dotenv from "dotenv";
 import { logger } from "./Middleware/logger";
+import userRoutes from "./routes/userRoutes";
+
 
 dotenv.config();
 
@@ -9,6 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(logger);
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 3000;
 
