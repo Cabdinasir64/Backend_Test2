6b99236f84c3af45678717469446949835369b60
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-30 09:37:42 +0300

    Enhance createUser with validation and password hashing
    - Integrated ensureUsernameEmailPasswordOK to validate username, email, and password before creating a user.
    - Added password hashing using hashPassword before saving the user.
    - Added optional role parameter with default "user".
    - getUsers, updateUser, and deleteUser functions remain unchanged.

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,9 +1,20 @@
 import User, { IUser } from "../models/User";
+import { hashPassword } from '../utils/hash'
+import { ensureUsernameEmailPasswordOK } from "../utils/validation";
 
-export const createUser = async (name: string, email: string, password: string): Promise<IUser> => {
-    const user = new User({ name, email, password });
+export const createUser = async (name: string, email: string, password: string, role: "admin" | "user" = "user"): Promise<{ user?: IUser; errors?: string[] }> => {
+
+    const validation = await ensureUsernameEmailPasswordOK(name, email, password);
+    if (!validation.valid) {
+        return { errors: validation.errors };
+    }
+
+    const hashpass = await hashPassword(password);
+
+    const user = new User({ name, email, password: hashpass, role });
     await user.save();
-    return user;
+
+    return { user };
 };
 
 export const getUsers = async (): Promise<IUser[]> => {
