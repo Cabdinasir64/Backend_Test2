bf95062f3e70e5baf576073db0f74bfc1cc614d8
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-30 09:56:17 +0300

    i make update cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { connectDB } from "./db";
 import dotenv from "dotenv";
 import { logger } from "./Middleware/logger";
@@ -7,7 +8,15 @@ import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 
+
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:3001",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use(logger);
