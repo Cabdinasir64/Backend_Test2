5ea12a49841408b29203f5cbd67b7329ba2e3c1a
Cabdinasir64 <cabdinasira64@gmail.com>
2025-10-03 16:57:14 +0300

    Add userRoutes2 with register, login, and verify endpoints
    - Created /register route for user registration using createUser controller.
    - Created /login route for user authentication using loginUser controller.
    - Created /verify route to handle user verification using verifyUser controller.

---
+++ src/routes/userroutes2.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createUser, loginUser, verifyUser } from "../controllers/usercontrollers2";
+
+const router = Router();
+
+router.post("/register/", createUser);
+
+router.post("/login", loginUser);
+
+router.post("/verify", verifyUser);
+
+export default router;
