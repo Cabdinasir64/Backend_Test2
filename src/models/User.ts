82a31d382c2d501c44f4be5faadb684e17952eae
Cabdinasir64 <cabdinasira64@gmail.com>
2025-10-04 18:30:15 +0300

    i make update and i add the all of them code line comment

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,28 +1,28 @@
-import { Schema, model, Document } from "mongoose";
+// import { Schema, model, Document } from "mongoose";
 
-export interface IUser extends Document {
-    name: string;
-    email: string;
-    password: string;
-    role: "admin" | "user";
-}
+// export interface IUser extends Document {
+//     name: string;
+//     email: string;
+//     password: string;
+//     role: "admin" | "user";
+// }
 
-const UserSchema = new Schema<IUser>(
-    {
-        name: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        role: { type: String, enum: ["admin", "user"], default: "user", required: true },
-    },
-    {
-        strict: true,
-        timestamps: true
-    },
-);
+// const UserSchema = new Schema<IUser>(
+//     {
+//         name: { type: String, required: true },
+//         email: { type: String, required: true, unique: true },
+//         password: { type: String, required: true },
+//         role: { type: String, enum: ["admin", "user"], default: "user", required: true },
+//     },
+//     {
+//         strict: true,
+//         timestamps: true
+//     },
+// );
 
-UserSchema.index({ role: 1 });
-UserSchema.index({ name: "text" });
-UserSchema.index({ name: 1 });
+// UserSchema.index({ role: 1 });
+// UserSchema.index({ name: "text" });
+// UserSchema.index({ name: 1 });
 
-const User = model<IUser>("Users", UserSchema);
-export default User;
+// const User = model<IUser>("Users", UserSchema);
+// export default User;
