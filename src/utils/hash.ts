7fe74e9aa4507b3c71dacd2aaf6ab450c889f979
Cabdinasir64 <cabdinasira64@gmail.com>
2025-10-09 16:58:00 +0300

    i make update

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -5,6 +5,7 @@ export const hashPassword = async (password: string): Promise<string> => {
     const hashed = await bcrypt.hash(password, salt);
     return hashed;
 };
+
 export const comparePassword = async (password: string, hashed: string): Promise<boolean> => {
   return bcrypt.compare(password, hashed);
 };
