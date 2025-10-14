ef20903a82793d09408ca6c7d3c79a69709283b6
Cabdinasir64 <cabdinasira64@gmail.com>
2025-10-14 11:07:14 +0300

    feat(post): add image upload functionality to post creation and update routes

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,20 +1,15 @@
 import { Router } from "express";
 import { authenticateToken } from "../Middleware/auth";
-import {
-    addPost,
-    updatePost,
-    deletePost,
-    getAllPosts,
-    getPostById
-} from "../controllers/posterControllers";
+import { upload } from "../utils/cloudinary";
+import { addPost, updatePost, deletePost, getAllPosts, getPostById, } from "../controllers/posterControllers";
 
 const router = Router();
 
 router.get("/", getAllPosts);
-router.get("/:id", getPostById); 
+router.get("/:id", getPostById);
 
-router.post("/", authenticateToken, addPost); 
-router.put("/:id", authenticateToken, updatePost);
+router.post("/", authenticateToken, upload.single("image"), addPost);
+router.put("/:id", authenticateToken, upload.single("image"), updatePost);
 router.delete("/:id", authenticateToken, deletePost);
 
 export default router;
